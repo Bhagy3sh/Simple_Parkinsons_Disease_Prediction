75940539e4cb5acb8ad5f0521eae89027620fd19
Bhagy3sh <141747782+Bhagy3sh@users.noreply.github.com>
2023-12-23 20:08:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -91,4 +91,4 @@ def prediction():
             return render_template("NotParkinsons.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
